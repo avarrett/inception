fd7ed804fb0c0c38f6018707b269c8fb902c5df1
avarrett <avarretta@gmail.com>
2026-08-20 15:40:11 +0200

    changed certif creation

--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-until nc -z wordpress 9000; do 
+if [ ! -f /etc/nginx/ssl/inception.crt ]; then
+	mkdir -p /etc/nginx/ssl
+	openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+		-keyout /etc/nginx/ssl/inception.key \
+		-out /etc/nginx/ssl/inception.crt \
+		-subj "/C=CH/ST=Vaud/L=Renens/O=42/CN=${DOMAIN_NAME}"
+fi
+
+until nc -z wordpress 9000; do
 	sleep 1;
 done
 
